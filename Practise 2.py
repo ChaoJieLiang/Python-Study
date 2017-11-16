904c7d5b5b7a421a2e2e0cd46bc4c1600108f4d9
Peter Liang <xxjxxl_paranoia@hotmail.com>
2017-11-16 17:09:59 +0800

    Python practise new added
    New added

---
+++ Practise 2.py
@@ -0,0 +1,28 @@
+'''
+# C2.1
+message = "Practise 2-1"
+print(message)
+
+# C2.2
+message = "Practise 2-2"
+print(message)
+message = "Practise 2-2 renew"
+print(message)
+'''
+
+# C2.3
+input_name = "Eric"
+print("Hello " + input_name.title() + ", would you like to learn some Python today?")
+
+# C2.4
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name.lower())
+print(full_name.upper())
+print(full_name.title())
+
+# C2.5
+name = "Albert Einstein"
+word = "A person who never made a mistake never tried anything new."
+print(name.title() + 'once said, "' + word + '"')
